e8a9d4ed843bbaa514e7c51afd2448c3d488ccdc
Pial Kanti Samadder <pialkanti2012@gmail.com>
2021-09-19 20:50:41 +0600

    Added Name field in Membership type and migration for populating name field

---
+++ MovieRentalService/Migrations/202109191445141_PopulateMembershipTypeName.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieRentalService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipType SET NAME = 'Pay as You Go' where Id = 1");
+            Sql("UPDATE MembershipType SET NAME = 'Monthly' where Id = 2");
+            Sql("UPDATE MembershipType SET NAME = 'Quarterly' where Id = 3");
+            Sql("UPDATE MembershipType SET NAME = 'Annual' where Id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- MovieRentalService/Models/MembershipType.cs
+++ MovieRentalService/Models/MembershipType.cs
@@ -8,6 +8,7 @@ namespace MovieRentalService.Models
     public class MembershipType
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public double SignUpFee { get; set; }
         public int DurationInMonths { get; set; }
         public double DiscountRate { get; set; }
