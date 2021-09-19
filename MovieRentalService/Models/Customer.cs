f66abef43c0136be4420351db11e8d697830042e
Pial Kanti Samadder <pialkanti2012@gmail.com>
2021-09-19 21:21:41 +0600

    Added birthdate to customer table

--- MovieRentalService/Models/Customer.cs
+++ MovieRentalService/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace MovieRentalService.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        
+        public DateTime? BirthDate { get; set; }
         public MembershipType MembershipType { get; set; }
         public int MembershipTypeId { get; set; }
     }
