677d90ac74e6bd5c255e70eeaa7076cce19cb5b5
Pial Kanti Samadder <pialkanti2012@gmail.com>
2021-09-19 21:35:19 +0600

    Added Genre to movies table

---
+++ MovieRentalService/Migrations/202109191530148_PopulateGenres.cs
@@ -0,0 +1,26 @@
+﻿namespace MovieRentalService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateGenres : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Genre (Name) VALUES ('Action')");
+            Sql("INSERT INTO Genre (Name) VALUES ('Family')");
+            Sql("INSERT INTO Genre (Name) VALUES ('Romance')");
+            Sql("INSERT INTO Genre (Name) VALUES ('Comedy')");
+            Sql("INSERT INTO Genre (Name) VALUES ('Science fiction')");
+        }
+        
+        public override void Down()
+        {
+            Sql("DELETE FROM Genre WHERE Id = '1'");
+            Sql("DELETE FROM Genre WHERE Id = '2'");
+            Sql("DELETE FROM Genre WHERE Id = '3'");
+            Sql("DELETE FROM Genre WHERE Id = '4'");
+            Sql("DELETE FROM Genre WHERE Id = '5'");
+        }
+    }
+}
--- MovieRentalService/Models/Genre.cs
+++ MovieRentalService/Models/Genre.cs
@@ -5,10 +5,9 @@ using System.Web;
 
 namespace MovieRentalService.Models
 {
-    public class Movie
+    public class Genre
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Genre Genre { get; set; }
     }
 }
